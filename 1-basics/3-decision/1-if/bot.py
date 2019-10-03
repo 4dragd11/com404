79e11861bcc38be080d637ea169e2c656712e884
4dragd11 <4dragd11@solent.ac.uk>
2019-10-03 13:21:52 +0000

    Added code to demonstrate the use of an if statement.

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,10 @@
+# Check whether first number is bigger than the second number.
+# Assume first_number and second_number are existing variables.
+print("Please enter a first number")
+first_number = int(input())
+print("Please enter a second number")
+second_number = int(input())
+if (first_number > second_number):
+    print("First is bigger!")
+
+print("Done!")
